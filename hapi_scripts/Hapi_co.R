5ce40a58b2966771cce00196e37b20dd2141ac00
zwycooky <65067477+zwycooky@users.noreply.github.com>
2023-06-17 23:08:35 +0800

    Create Hapi_co.R

---
+++ hapi_scripts/Hapi_co.R
@@ -0,0 +1,18 @@
+library('Hapi')
+library('HMM')
+
+args=commandArgs(T)
+
+input <- args[1]
+output <- args[2]
+
+hapdat <- read.table(input,header=T, colClasses = c ("character"))
+hapdat <- hapdat[hapdat$confidence != "L" & hapdat$rate > 0.8,]
+
+hap <- hapdat[,c("hap1","hap2")]
+gmt <- hapdat[,grepl("[(J,DASZ)]",colnames(hapdat))]
+
+cvOutput <- hapiIdentifyCV(hap = hap, gmt = gmt)
+
+write.table(cvOutput,output,sep="\t",col.names=T,row.names=F,quote=F)
+
