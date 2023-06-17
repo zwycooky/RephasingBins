efbc7ac624b02d918a733be5198a8e8d4a4eae3d
zwycooky <65067477+zwycooky@users.noreply.github.com>
2023-06-17 23:32:25 +0800

    Update contig_Hapi_phasing.pl

--- hapi_scripts/contig_Hapi_phasing.pl
+++ hapi_scripts/contig_Hapi_phasing.pl
@@ -14,7 +14,7 @@ my @file = (split /,/,$vcf_list);
 
 foreach (@file) {
 	my $contig = (split /\./,(split /\//,$_)[-1])[0];
-	!system "Rscript $dir/scripts/Hapi_step_by_step_parallel.2.R $_ $dir/hapOut/$contig.hap.txt $dir" or warn "warnning with hapi phasing $contig:$!";
+	!system "Rscript $dir/hapi_scripts/Hapi_step_by_step_parallel.2.R $_ $dir/hapOut/$contig.hap.txt $dir" or warn "warnning with hapi phasing $contig:$!";
 	#print "Rscript $dir/scripts/Hapi_step_by_step_parallel.2.R $_ $dir/hapOut/$contig.hap.txt\n";
 	print "$contig DONE!\n";
 }
